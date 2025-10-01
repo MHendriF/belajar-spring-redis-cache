b82d4f591e637af87bdea5edd88ba16bd6fd3d9a
MHendriF <hendrifebriansyah28@gmail.com>
2025-10-01 17:58:06 +0800

    init project

---
+++ src/test/java/hendrifbs/cache/BelajarSpringRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package hendrifbs.cache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringRedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
